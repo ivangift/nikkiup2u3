6ac8c39ddb26ea3ed2c29603397bc9ca474a15fd
Yifan <yfchen@yfchen-macbookair2.roam.corp.google.com>
2015-05-28 11:39:52 +0800

    draw table

--- nikki.js
+++ nikki.js
@@ -1,3 +1,80 @@
+var types = ["头发", "连衣裙", "外套", "上装", "下装", "袜子", "鞋子", "饰品", "妆容"];
+
+// Clothes: name, type, simple, gorgeous, cute, mature, active, elegant, pure, sexy, cool, warm，extra
+var raw = [
+  ["默认粉毛", 0, "S", "", "A", "", "A", "", "A", "", "", "A", ""]
+];
+
+var clothes = function() {
+	var ret = [];
+  for (var i in raw) {
+  	ret.push(Clothes(clothes[i]));
+  }
+  return ret;
+}();
+
+function rating(a, b) {
+	realRating = a ? a : b;
+	switch (realRating) {
+		case "SS": return 5;
+		case "S": return 4;
+		case "A": return 3;
+		case "B": return 2;
+		case "C": return 1;
+	}
+}
+
+// parses a csv row into object
+Clothes = function(csv) {
+	return {
+		name: csv[0],
+		type: types[csv[1]],
+		simple: rating(csv[2], csv[3]),
+		cute: rating(csv[4], csv[5]),
+		active: rating(csv[6], csv[7]),
+		pure: rating(csv[8], csv[9]),
+		cool: rating(csv[10], csv[11]),
+		extra: csv[12],
+		toCsv = function() {
+			
+		}
+	};
+}
+
+// for table use
+function table(tdata) {
+	return "<table>" + tdata + "</table>";
+}
+
+function tr(tds) {
+	return "<tr>" + tds + "</tr>";
+}
+
+function td(data) {
+	return "<td>" + data + "</td>";
+}
+
+function row(piece) {
+	ret = ""
+	for (var i in piece) {
+		ret += td(piece[i]);
+	}
+	return tr(ret);
+}
+
+function list(rows) {
+	ret = ""
+	for (var i in rows) {
+		ret += row(rows[i]);
+	}
+	return table(ret);
+}
+
+function drawTable(data, div) {
+  container = document.getElementById(div);
+  container.innerHTML = list(data);
+}
+
 function getCookie(c_name) {
 	if (document.cookie.length>0) { 
 		c_start=document.cookie.indexOf(c_name + "=")
@@ -39,4 +116,3 @@ function loadCookie() {
 	}
 	return "";
 	*/
-}
